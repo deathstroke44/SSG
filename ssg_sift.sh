53ca943bd8d5de01bad3240be97cbe8d03454b4c
samin_yeaser <samin.yeaser@tigerit.com>
2023-12-22 01:46:57 -0600

    updated script

--- ssg_sift.sh
+++ ssg_sift.sh
@@ -4,7 +4,7 @@ INDEX_PATH=/data/kabir/similarity-search/models/SSG/indexes
 LOG_PATH=/data/kabir/similarity-search/models/SSG/indexes/logs
 
 
-taskset --cpu-list 62-62 ./build/tests/test_nndescent $SIFT_PATH/sift_base.fvecs $INDEX_PATH/sift_100nn.knng 100 100 12 10 100 > $LOG_PATH/efannlogsift1.txt
+taskset --cpu-list 62-62 ./data/kabir/similarity-search/models/efanna_graph/tests/test_nndescent $SIFT_PATH/sift_base.fvecs $INDEX_PATH/sift_100nn.knng 100 100 12 10 100 > $LOG_PATH/efannlogsift1.txt
 
 taskset --cpu-list 62-62 ./build/tests/test_ssg_index $SIFT_PATH/sift_base.fvecs $INDEX_PATH/sift_100nn.knng 100 50 60 $INDEX_PATH/sift.ssg > $LOG_PATH/ssgbuildlogsift1.txt
 
