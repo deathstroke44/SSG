20be82c2cd0878d36c378bd225be8fb9c7ec3dec
samin_yeaser <samin.yeaser@tigerit.com>
2023-12-23 23:54:43 -0600

    added gist script for SSG

---
+++ ssg_gist.sh
@@ -0,0 +1,14 @@
+DATASET_PATH=/data/kabir/similarity-search/dataset
+SIFT_PATH=$DATASET_PATH/gist
+INDEX_PATH=/data/kabir/similarity-search/models/SSG/indexes
+LOG_PATH=/data/kabir/similarity-search/models/SSG/logs
+
+cd /data/kabir/similarity-search/models/efanna_graph/tests/
+
+taskset --cpu-list 62-62 ./test_nndescent $SIFT_PATH/gist_base.fvecs $INDEX_PATH/gist_100nn.knng 100 100 12 10 100 > $LOG_PATH/efannloggist1.txt
+
+cd /data/kabir/similarity-search/models/SSG/
+
+taskset --cpu-list 62-62 ./build/tests/test_ssg_index $SIFT_PATH/gist_base.fvecs $INDEX_PATH/gist_100nn.knng 100 50 60 $INDEX_PATH/gist.ssg > $LOG_PATH/ssgbuildloggist1.txt
+
+taskset --cpu-list 62-62 ./build/tests/test_ssg_optimized_search $SIFT_PATH/gist_base.fvecs $SIFT_PATH/gist_query.fvecs $INDEX_PATH/gist.ssg 200 100 ssgsearchqueryresultgist1-1.txt 13 $SIFT_PATH/gist_groundtruth.ivecs > $LOG_PATH/ssgsearchloggist1-1.txt
