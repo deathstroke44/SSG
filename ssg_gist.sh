89bb0a2037f29a07be3179931cc6e6b57c819c1e
samin_yeaser <samin.yeaser@tigerit.com>
2024-01-18 02:59:48 -0500

    Removed cpu id from scripts

--- ssg_gist.sh
+++ ssg_gist.sh
@@ -5,10 +5,10 @@ LOG_PATH=/data/kabir/similarity-search/models/SSG/logs
 
 cd /data/kabir/similarity-search/models/efanna_graph/tests/
 
-taskset --cpu-list 62-62 ./test_nndescent $SIFT_PATH/gist_base.fvecs $INDEX_PATH/gist_100nn.knng 100 100 12 10 100 > $LOG_PATH/efannloggist1.txt
+./test_nndescent $SIFT_PATH/gist_base.fvecs $INDEX_PATH/gist_100nn.knng 100 100 12 10 100 > $LOG_PATH/efannloggist1.txt
 
 cd /data/kabir/similarity-search/models/SSG/
 
-taskset --cpu-list 62-62 ./build/tests/test_ssg_index $SIFT_PATH/gist_base.fvecs $INDEX_PATH/gist_100nn.knng 100 50 60 $INDEX_PATH/gist.ssg > $LOG_PATH/ssgbuildloggist1.txt
+./build/tests/test_ssg_index $SIFT_PATH/gist_base.fvecs $INDEX_PATH/gist_100nn.knng 100 50 60 $INDEX_PATH/gist.ssg > $LOG_PATH/ssgbuildloggist1.txt
 
-taskset --cpu-list 62-62 ./build/tests/test_ssg_optimized_search $SIFT_PATH/gist_base.fvecs $SIFT_PATH/gist_query.fvecs $INDEX_PATH/gist.ssg 200 100 ssgsearchqueryresultgist1-1.txt 13 $SIFT_PATH/gist_groundtruth.ivecs > $LOG_PATH/ssgsearchloggist1-1.txt
+./build/tests/test_ssg_optimized_search $SIFT_PATH/gist_base.fvecs $SIFT_PATH/gist_query.fvecs $INDEX_PATH/gist.ssg 200 100 ssgsearchqueryresultgist1-1.txt 13 $SIFT_PATH/gist_groundtruth.ivecs > $LOG_PATH/ssgsearchloggist1-1.txt
--- ssg_sun.sh
+++ ssg_sun.sh
@@ -5,10 +5,10 @@ LOG_PATH=/data/kabir/similarity-search/models/SSG/logs
 
 cd /data/kabir/similarity-search/models/efanna_graph/tests/
 
-taskset --cpu-list 66-66 ./test_nndescent $SIFT_PATH/base.fvecs $INDEX_PATH/sun_100nn.knng 20 20 12 10 100 > $LOG_PATH/efannlogsun1.txt
+./test_nndescent $SIFT_PATH/base.fvecs $INDEX_PATH/sun_100nn.knng 20 20 12 10 100 > $LOG_PATH/efannlogsun1.txt
 
 cd /data/kabir/similarity-search/models/SSG/
 
-taskset --cpu-list 66-66 ./build/tests/test_ssg_index $SIFT_PATH/base.fvecs $INDEX_PATH/sun_100nn.knng 20 50 60 $INDEX_PATH/sun.ssg > $LOG_PATH/ssgbuildlogsun1.txt
+./build/tests/test_ssg_index $SIFT_PATH/base.fvecs $INDEX_PATH/sun_100nn.knng 20 50 60 $INDEX_PATH/sun.ssg > $LOG_PATH/ssgbuildlogsun1.txt
 
-taskset --cpu-list 66-66 ./build/tests/test_ssg_optimized_search $SIFT_PATH/base.fvecs $SIFT_PATH/query.fvecs $INDEX_PATH/sun.ssg 100 20 ssgsearchqueryresultsun1-1.txt 13 $SIFT_PATH/groundtruth.ivecs > $LOG_PATH/ssgsearchlogsun1-1.txt
+./build/tests/test_ssg_optimized_search $SIFT_PATH/base.fvecs $SIFT_PATH/query.fvecs $INDEX_PATH/sun.ssg 100 20 ssgsearchqueryresultsun1-1.txt 13 $SIFT_PATH/groundtruth.ivecs > $LOG_PATH/ssgsearchlogsun1-1.txt
