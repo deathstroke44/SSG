fcf1bc8efe41e78caaeb93dabc81c08b4162eae8
samin_yeaser <samin.yeaser@tigerit.com>
2024-03-05 03:32:11 -0500

    new scripts

--- ssg_sun.sh
+++ ssg_sun.sh
@@ -1,14 +1,45 @@
 DATASET_PATH=/data/kabir/similarity-search/dataset
-SIFT_PATH=$DATASET_PATH/sun
+DATASET_NAME=sun
+ID=2
+DATA_PATH=$DATASET_PATH/${DATASET_NAME}
+BASE_PATH=$DATA_PATH/base.fvecs
+QUERY_PATH=$DATA_PATH/query.fvecs
+GROUNDTRUTH_PATH=$DATA_PATH/groundtruth.ivecs
 INDEX_PATH=/data/kabir/similarity-search/models/SSG/indexes
 LOG_PATH=/data/kabir/similarity-search/models/SSG/logs
+EFANNA_LOG=$LOG_PATH/efannlog${DATASET_NAME}${ID}.txt
+SSG_LOG=$LOG_PATH/ssgbuildlog${DATASET_NAME}${ID}.txt
+EFANNA_GRAPH=$INDEX_PATH/${DATASET_NAME}${ID}_100nn.knng
+SSG_GRAPH=$INDEX_PATH/${DATASET_NAME}${ID}.ssg
 
 cd /data/kabir/similarity-search/models/efanna_graph/tests/
-
-./test_nndescent $SIFT_PATH/base.fvecs $INDEX_PATH/sun_100nn.knng 20 20 12 10 100 > $LOG_PATH/efannlogsun1.txt
-
+./test_nndescent $BASE_PATH $EFANNA_GRAPH 100 100 12 10 100 > $EFANNA_LOG
 cd /data/kabir/similarity-search/models/SSG/
+./build/tests/test_ssg_index $BASE_PATH $EFANNA_GRAPH 100 50 60 $SSG_GRAPH > $SSG_LOG
 
-./build/tests/test_ssg_index $SIFT_PATH/base.fvecs $INDEX_PATH/sun_100nn.knng 20 50 60 $INDEX_PATH/sun.ssg > $LOG_PATH/ssgbuildlogsun1.txt
+SUB_ID=1
+SEARCH_LOG=$LOG_PATH/ssgsearchlog${DATASET_NAME}${ID}-${SUB_ID}.txt
+SEARCH_RESULT_LOG=ssgsearchqueryresult${DATASET_NAME}${ID}-${SUB_ID}.txt
 
-./build/tests/test_ssg_optimized_search $SIFT_PATH/base.fvecs $SIFT_PATH/query.fvecs $INDEX_PATH/sun.ssg 100 20 ssgsearchqueryresultsun1-1.txt 13 $SIFT_PATH/groundtruth.ivecs > $LOG_PATH/ssgsearchlogsun1-1.txt
+SEARCH_L=40
+SEARCH_K=20
+
+./build/tests/test_ssg_optimized_search $BASE_PATH $QUERY_PATH $SSG_GRAPH $SEARCH_L $SEARCH_K $SEARCH_RESULT_LOG 13 $GROUNDTRUTH_PATH > $SEARCH_LOG
+
+SUB_ID=2
+SEARCH_LOG=$LOG_PATH/ssgsearchlog${DATASET_NAME}${ID}-${SUB_ID}.txt
+SEARCH_RESULT_LOG=ssgsearchqueryresult${DATASET_NAME}${ID}-${SUB_ID}.txt
+
+SEARCH_L=60
+SEARCH_K=20
+
+./build/tests/test_ssg_optimized_search $BASE_PATH $QUERY_PATH $SSG_GRAPH $SEARCH_L $SEARCH_K $SEARCH_RESULT_LOG 13 $GROUNDTRUTH_PATH > $SEARCH_LOG
+
+SUB_ID=3
+SEARCH_LOG=$LOG_PATH/ssgsearchlog${DATASET_NAME}${ID}-${SUB_ID}.txt
+SEARCH_RESULT_LOG=ssgsearchqueryresult${DATASET_NAME}${ID}-${SUB_ID}.txt
+
+SEARCH_L=80
+SEARCH_K=20
+
+./build/tests/test_ssg_optimized_search $BASE_PATH $QUERY_PATH $SSG_GRAPH $SEARCH_L $SEARCH_K $SEARCH_RESULT_LOG 13 $GROUNDTRUTH_PATH > $SEARCH_LOG
